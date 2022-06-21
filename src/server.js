7d8e8b6d8ba8fee727f699d9d08844bc1bf9f4c5
Felipe Rosas <contato@eufelipe.com>
2022-06-21 00:57:02 -0300

    chore: add readme

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 /*
 
   - TODO 1: Criar um servidor que recebe requisições HTTP na porta 8000 e responde com o conteúdo de um arquivo HTML.
-  - TODO 2: Se se for acessada a URL http://localhost:8000/about deve mostrar o conteúdo da pagina que esta em `pages/about.html`
-  - TODO 3: Se for acessada a URL http://localhost:8000/ ou http://localhost:8000/home deve mostrar o conteúdo da pagina que esta em `pages/index.html`
-  - TODO 4: Se for acessada qualquer outro caminho deve mostrar o conteúdo da pagina que esta em `pages/404.html`
+  - TODO 2: Se for acessada a URL http://localhost:8000/about deve mostrar o conteúdo da pagina `pages/about.html`
+  - TODO 3: Se for acessada a URL http://localhost:8000/ ou http://localhost:8000/home deve mostrar o conteúdo da pagina `pages/index.html`
+  - TODO 4: Se for acessada qualquer outro caminho deve mostrar o conteúdo da pagina `pages/404.html`
  
 
   OBS: Deve ser utilizado apenas os módulos nativos do NODE (http, path, fs, etc), nada de instalar outras libs ( ˘︹˘ )
